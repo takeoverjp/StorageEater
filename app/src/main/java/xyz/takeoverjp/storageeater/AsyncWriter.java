8f10f519376e8e412fec17d8c7534e3ea06218ec
Kondo Takeo <takeoverjp@gmail.com>
2019-11-03 13:13:13 +0900

    StorageEater initial commit.

---
+++ app/src/main/java/xyz/takeoverjp/storageeater/AsyncWriter.java
@@ -0,0 +1,89 @@
+package xyz.takeoverjp.storageeater;
+
+import android.os.AsyncTask;
+import android.os.storage.StorageManager;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class AsyncWriter extends AsyncTask<Integer, Integer, Integer> {
+    private File dir;
+    private StorageManager sm;
+    private ProgressBar progressBar;
+    private ProgressBar loadingCircle;
+    private TextView freeSpaceView;
+    private TextView usableSpaceView;
+    private TextView allocatableBytesView;
+
+    public AsyncWriter(StorageManager sm, File dir, ProgressBar progressBar, ProgressBar loadingCircle,
+                       TextView freeSpaceView, TextView usableSpaceView, TextView allocatableBytesView) {
+        super();
+        this.sm = sm;
+        this.dir = dir;
+        this.progressBar = progressBar;
+        this.loadingCircle = loadingCircle;
+        this.freeSpaceView = freeSpaceView;
+        this.usableSpaceView = usableSpaceView;
+        this.allocatableBytesView = allocatableBytesView;
+    }
+
+    private void updateView () {
+        long total = dir.getTotalSpace();
+        long free = dir.getFreeSpace();
+        int ratio = (int)((total - free) * 100 / total);
+        long usable = dir.getUsableSpace();
+        freeSpaceView.setText(String.format("%,d byte", free));
+        usableSpaceView.setText(String.format("%,d byte", usable));
+        progressBar.setProgress(ratio, true);
+        try {
+            allocatableBytesView.setText(String.format("%,d byte", sm.getAllocatableBytes(sm.UUID_DEFAULT)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onPreExecute() {
+        loadingCircle.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected Integer doInBackground(Integer... value) {
+        long free = dir.getFreeSpace();
+        long write_size = free * 120 / 1024 / 100;
+
+        try {
+            FileOutputStream file = new FileOutputStream(dir + "/garbage", true);
+            try {
+                byte[] bytes = new byte[Math.toIntExact(write_size)];
+                for (int i = 0; i < 100; i++) {
+                    for (int j = 0; j < 1024; j++) {
+                        file.write(bytes);
+                    }
+                    updateView();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                file.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Integer result) {
+        updateView();
+        loadingCircle.setVisibility(View.INVISIBLE);
+    }
+}
---
+++ app/src/main/java/xyz/takeoverjp/storageeater/MainActivity.java
@@ -0,0 +1,92 @@
+package xyz.takeoverjp.storageeater;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.storage.StorageManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    final private String LOG_TAG = "MainActivity";
+    private File filesDir;
+    private AsyncWriter writer;
+    private StorageManager sm;
+    private ProgressBar loadingCircle;
+    private ProgressBar progressBar;
+    private TextView totalSpaceView;
+    private TextView freeSpaceView;
+    private TextView usableSpaceView;
+    private TextView allocatableBytesView;
+
+    private void updateView () {
+        long total = filesDir.getTotalSpace();
+        long free = filesDir.getFreeSpace();
+        int ratio = (int)((total - free) * 100 / total);
+        long usable = filesDir.getUsableSpace();
+        freeSpaceView.setText(String.format("%,d byte", free));
+        usableSpaceView.setText(String.format("%,d byte", usable));
+        try {
+            allocatableBytesView.setText(String.format("%,d byte", sm.getAllocatableBytes(sm.UUID_DEFAULT)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        progressBar.setProgress(ratio, true);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        filesDir = getFilesDir();
+        TextView filesDirView = (TextView)findViewById(R.id.filesDirValueView);
+        filesDirView.setText(filesDir.getAbsolutePath());
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setMax(100);
+
+        loadingCircle = (ProgressBar)findViewById(R.id.loadingCircle);
+
+        totalSpaceView = (TextView)findViewById(R.id.totalSpaceValueView);
+        totalSpaceView.setText(String.format("%,d byte", filesDir.getTotalSpace()));
+
+        freeSpaceView = (TextView)findViewById(R.id.freeSpaceValueView);
+        freeSpaceView.setText(String.format("%,d byte", filesDir.getFreeSpace()));
+
+        usableSpaceView = (TextView)findViewById(R.id.usableSpaceValueView);
+        usableSpaceView.setText(String.format("%,d byte", filesDir.getUsableSpace()));
+
+        sm = (StorageManager)getBaseContext().getSystemService(Context.STORAGE_SERVICE);
+        allocatableBytesView = (TextView)findViewById(R.id.allocatableBytesValueView);
+        try {
+            allocatableBytesView.setText(String.format("%,d byte", sm.getAllocatableBytes(StorageManager.UUID_DEFAULT)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        updateView();
+    }
+
+    public void onClickEat(View v) {
+        Log.v(LOG_TAG, "Eat button clicked");
+        writer = new AsyncWriter(sm, filesDir, progressBar, loadingCircle,
+                freeSpaceView, usableSpaceView, allocatableBytesView);
+        writer.execute();
+    }
+
+    public void onClickClear(View v) {
+        Log.v(LOG_TAG, "Clear button clicked");
+        File file = new File(filesDir + "/garbage");
+        if (file.exists()) {
+            file.delete();
+        }
+        updateView();
+    }
+}
